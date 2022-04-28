d829759629c2a16fdc1b1bd726d2286ca021320f
nanonav77 <navrojd77@gmail.com>
2022-04-28 13:06:34 -0600

    add server package

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+
+    res.send("hello world nanito");
+
+});
+
+app.listen(3001, () => {
+
+    console.log('running on port 3001');
+
+});
